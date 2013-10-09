afcec3038228e468e030a7b23c5b5483fac73e70
Tim <tim.blonski@gmail.com>
2013-10-09 14:53:03 -0400

    re-worked example

--- js/app.js
+++ js/app.js
@@ -1,4 +1,41 @@
-App = Ember.Application.create({});
+App = Ember.Application.create();
+
+
+App.Router.map(function() {
+  this.resource('about');
+  this.resource('posts', function(){
+    this.resource('post', { path: ':post_id'});
+  });
+});
+
+
+App.PostsRoute = Ember.Route.extend({
+  model: function(){
+    return posts;
+  }
+});
+
+
+App.PostRoute = Ember.Route.extend({
+  model: function(params) {
+    return posts.findBy('id', params.post_id);
+  }
+});
+
+
+App.PostController = Ember.ObjectController.extend({
+  isEditing: false,
+
+  actions: {
+    edit: function(){
+      this.set('isEditing', true);
+    },
+
+    doneEditing: function(){
+      this.set('isEditing', false);
+    }
+  }
+});
 
 var posts = [{
   id: '1',
@@ -13,47 +50,17 @@ var posts = [{
   author: { name: "d2h" },
   date: new Date('12-24-2012'),
   excerpt: "My [appearance on the Ruby Rogues podcast](http://rubyrogues.com/056-rr-david-heinemeier-hansson/) recently came up for discussion again on the private Parley mailing list.",
-  body: "A long list of topics were raised and I took a time to ramble at large about all of them at once. Apologies for not taking the time to be more succinct, but at least each topic has a header so you can skip stuff you don't care about.\n\n### Maintainability\n\nIt's simply not true to say that I don't care about maintainability. I still work on the oldest Rails app in the world."  
+  body: "A long list of topics were raised and I took a time to ramble at large about all of them at once. Apologies for not taking the time to be more succinct, but at least each topic has a header so you can skip stuff you don't care about.\n\n### Maintainability\n\nIt's simply not true to say that I don't care about maintainability. I still work on the oldest Rails app in the world."
 }];
 
-App.Router.map(function() {
-  this.resource('about');
-  this.resource('posts', function() {
-    this.resource('post', { path: ':post_id' });
-  });
+
+Ember.Handlebars.helper('format-date', function(date) {
+  return moment(date).fromNow();
 });
 
-App.PostsRoute = Ember.Route.extend({
-  model: function() {
-    return posts;
-  }
-});
-
-App.PostRoute = Ember.Route.extend({
-  model: function(params) {
-    return posts.findBy('id', params.post_id);
-  }
-});
-
-App.PostController = Ember.ObjectController.extend({
-  isEditing: false,
-
-  edit: function() {
-    this.set('isEditing', true);
-  },
-
-  doneEditing: function() {
-    this.set('isEditing', false);
-    this.get('store').commit();
-  }
-});
 
 var showdown = new Showdown.converter();
 
 Ember.Handlebars.helper('format-markdown', function(input) {
   return new Handlebars.SafeString(showdown.makeHtml(input));
-});
-
-Ember.Handlebars.helper('format-date', function(date) {
-  return moment(date).fromNow();
 });
